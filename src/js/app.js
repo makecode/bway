47965b74ec4f14d5ebaab5152067415319286a1a
Rita Akhmadulina <rita.akhmadulina@gmail.com>
2018-12-26 23:22:28 +0200

    add new search

--- src/js/app.js
+++ src/js/app.js
@@ -1,59 +1,58 @@
-//=include libs/jquery.min.js
-//=include libs/owl.carousel.min.js
 'use strict';
 
+const owlConfig = {
+  autoplay: true,
+  autoplayTimeout: 7000,
+  items: 3,
+  margin: 20,
+  loop: true,
+  dots: true,
+  nav: false
+};
+
 $( document ).ready(() => {
-  const tabsSwitcher = function(tabsArray, contentArray) {
-    tabsArray.map(function (i, el) {
-      $(el).click(function () {
-        let activeIndex = i;
-        contentArray.map(function(contentIndex, content) {
-          if( activeIndex === contentIndex ) {
-            $(content).addClass('is-visible')
-          } else (
-            $(content).removeClass('is-visible')
-          )
+  const tabsSwitcher = () => {
+    const tabs = $('.tabs'),
+          tabsEl = tabs.find('.tabs__el'),
+          tabsContent = tabs.find('.tabs__content');
+
+    const tabsToggle = function(tabsArray, contentArray) {
+      tabsArray.map(function (i, el) {
+        $(el).click(function () {
+          let activeIndex = i;
+          contentArray.map(function(contentIndex, content) {
+            if( activeIndex === contentIndex ) {
+              $(content).addClass('is-visible')
+            } else (
+              $(content).removeClass('is-visible')
+            )
+          });
+          tabsArray.map(function(i, tab) {
+            $(this).removeClass('is-active');
+          });
+          $(this).addClass('is-active');
         });
-        tabsArray.map(function(i, tab) {
-          $(this).removeClass('is-active');
-        });
-        $(this).addClass('is-active');
       });
-    });
+    };
+
+    tabsToggle(tabsEl, tabsContent);
   };
 
-  const tabs = $('.tabs'),
-      tabsEl = tabs.find('.tabs__el'),
-      tabsContent = tabs.find('.tabs__content');
-  tabsSwitcher(tabsEl, tabsContent);
+  $('.owl-carousel').owlCarousel(owlConfig);
 
-  $('.owl-carousel').owlCarousel({
-    autoplay: true,
-    autoplayTimeout: 7000,
-    items: 3,
-    margin: 20,
-    loop: true,
-    pagination: false,
-    nav: true,
-    // navigationText: ['<div class="left"></div>', '<div class="right"></div>']
-    navText: ['<img class="btn-prev" src="../img/arrow-left.png" />','<img class="btn-next" src="../img/arrow-right.png" />']
-  });
-
-  // $('.aside__title').click(function () {
-  //   $('.filter__list').addClass('is-open');
-  // });
-
-  $(window).scroll(function(){
+  $(window).scroll(function() {
     const sticky = $('.header'),
-      scroll = $(window).scrollTop();
+          body = $('body'),
+          scroll = $(window).scrollTop();
 
-    if (scroll >= 100) sticky.addClass('fixed');
-    else sticky.removeClass('fixed');
+    if (scroll >= 30) {
+      sticky.addClass('fixed');
+      body.addClass('without-header');
+    } else {
+      sticky.removeClass('fixed');
+      body.removeClass('without-header');
+    }
   });
-
-  // TweenMax.fromTo('.header__left', 1, {x: -200, opacity: 0}, {x: 0, opacity: 1}, "-0.7");
-  // TweenMax.fromTo('.header__center', 1, {y: -100, opacity: 0}, {y: 0, opacity: 1});
-  // TweenMax.fromTo('.header__right', 1, {x: 200, opacity: 0}, {x: 0, opacity: 1}, "-0.7");
 
   $('<div class="quantity-nav"><div class="quantity-button quantity-up">+</div><div class="quantity-button quantity-down">-</div></div>').insertAfter('.quantity input');
   $('.quantity').each(function() {
@@ -88,4 +87,28 @@ $( document ).ready(() => {
 
   });
 
+  const search = () => {
+    const $search = $('#search'),
+          activeClass = 'opened';
+
+    $($search).on('click', () => {
+      const opened = $($search).hasClass(activeClass);
+      if (opened) return;
+      $($search).addClass(activeClass);
+    });
+  };
+
+  $(window).on('click', (e) => {
+    const $search = $('#search'),
+          $target = $(e.target);
+    const test = $($search).find($target);
+
+    if ( !test.length ) {
+      $($search).removeClass('opened');
+    }
+  });
+
+  // INITIALISATION
+  tabsSwitcher();
+  search();
 });
